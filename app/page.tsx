3d1a31d2f3cd4058f25cf7d3473fe321e4591651
Nathaniel Cowan <nathaniel.g.cowan@gmail.com>
2023-10-11 00:52:34 -0500

    the country

--- app/page.tsx
+++ app/page.tsx
@@ -1,111 +1,10 @@
 'use client'
 import React, { useState } from "react";
+import Datas from '../components/Arrays';
 
 export default function App() {
-  const [array, setArray] = useState([
-    {
-      nation: 'USA',
-      data: [
-        {
-          population: 35485000,
-          percentageofworld: 4.2,
-          date: "9 Oct 2023",
-          source: 'National annual projection',
-          fullname: "United States of America",
-          motto: "In God We Trust",
-          anthem: "The Star-Spangled Banner",
-          capital: "Washington, D.C",
-          largestcity: "New York City",
-          Officiallanguages: "none at the federal level",
-          nationallanguage: "English",
-          flag: 'flag',
-          demonym: "American",
-          government: "Federal presidential constitutional republic",
-          president: "Joe Biden",
-          vicePresident: "Kamala Harris",
-          houseSpeaker: "Vacant",
-          chiefJustice: "John Roberts",
-          gini: 39.4,
-          hdi: 0.921,
-          currency: "U.S. dollar",
-          dateFormat: "mm/dd/yyyy",
-          drivingSide: "right",
-          callingCode: "+1",
-          iso3166code: 'US',
-          internetTLD: 'us',
-          origin: [
-            {
-              nonHispanicOrLatino: 81.3,
-              hispanicOrLatino: 18.7
-            }
-          ],
-          religion: [{
-            christianity: [{
-              Protestantism: 40,
-              Catholicism: 21,
-              other: 2
-            }],
-            unaffiliated: 29,
-            Buddhism:1,
-            Hinduism: 1,
-            Islam: 1,
-            Judaism: 1,
-            Other: 2,
-            unanswered: 2
-          }],
-          timeZone: [{
-            timeZone: "	UTC−4 to −12, +10, +11",
-            summer: "UTC−4 to −10[g]"
-          }],
-          independencefromGreatBritain: [{
-            declaration: 'July 4, 1776',
-            confederation: 'March 1, 1781',
-            recognized: 'September 3, 1783',
-            constitution: 'June 21, 1788',
-            lastAmendment: 'May 5, 1992'
-          }],
-          area: [{
-            totalarea: 379674,
-            water: 4.66,
-            landarea: 3531905
-          }],
-          pop: [{
-            twentytwoestimate: 333287557,
-            twentyCensus: 331449281,
-            density: "87/sq mi"
-          }],
-          gdp: [{
-            year: 2023,
-            total: "$26.949 trillion",
-            perCapita: "$80412"
-          }],
-          legislature: [{
-            congress: 'Congress',
-            upperhouse: 'Senate',
-            lowerhouse: 'House of Representatives'
-          }],
-          ethnicgroups: [
-            {
-              white: 61.6,
-              black: 12.4,
-              asian: 6,
-              nativeamerican: 1.1,
-              pacificislander: 0.2,
-              twoormoreraces: 10.2,
-              other: 8.4
-            }
-          ],
-          region: [
-            {
-              state: [
-                { a: 'a'}
-              ]
-            }
-          ]
-        }]
-    },
-  ])
-
+  const [array, setArray] = useState(Datas)
+  console.log(Datas)
   return (
     <>
     {array.map((e, i) => {
@@ -119,6 +18,28 @@ export default function App() {
                 {e.percentageofworld}
                 {e.date}
                 {e.source}
+                {e.fullname}
+                {e.motto}
+                {e.anthem}
+                {e.capital}
+                {e.largestcity}
+                {e.officiallanguages}
+                {e.nationallanguage}
+                {e.flag}
+                {e.demonym}
+                {e.government}
+                {e.president}
+                {e.vicepresident}
+                {e.housespeaker}
+                {e.chiefjustice}
+                {e.gini}
+                {e.hdi}
+                {e.currency}
+                {e.dateformat}
+                {e.drivingside}
+                {e.callingcode}
+                {e.iso3166code}
+                {e.internettld}
                 {e.ethnicgroups.map((e,i) => {
                   return (
                     <div key={i}> 
@@ -127,7 +48,21 @@ export default function App() {
                     {e.asian},
                     {e.nativeamerican},
                     {e.pacificislander},
-                    {e.twoormoreraces}
+                    {e.twoormoreraces},
+                    {e.other}
+                    </div>
+                  )
+                })}
+                {e.ethnicgroups.map((e,i) => {
+                  return (
+                    <div key={i}> 
+                    {e.white}
+                    {e.black},
+                    {e.asian},
+                    {e.nativeamerican},
+                    {e.pacificislander},
+                    {e.twoormoreraces},
+                    {e.other}
                     </div>
                   )
                 })}
@@ -137,7 +72,35 @@ export default function App() {
                       {e.state.map((e,i) => {
                         return (
                           <div key={i}>
-                            {e.a}
+                            {e.name}
+                          </div>
+                        )
+                      })}
+                      {e.district.map((e,i) => {
+                        return (
+                          <div key={i}>
+                            {e.name}
+                          </div>
+                        )
+                      })}
+                      {e.territory.map((e,i) => {
+                        return (
+                          <div key={i}>
+                            {e.name}
+                          </div>
+                        )
+                      })}
+                      {e.uninhabitedterritories.map((e,i) => {
+                        return (
+                          <div key={i}>
+                            {e.name}
+                          </div>
+                        )
+                      })}
+                      {e.county.map((e,i) => {
+                        return (
+                          <div key={i}>
+                            {e.county}
                           </div>
                         )
                       })}
